8cfcb69375f12cff33d79503f49f54bc44a1ae9f
omega-beta <140123838+omega-beta@users.noreply.github.com>
2023-12-19 08:51:40 -0500

    Create key.js

---
+++ key.js
@@ -0,0 +1,17 @@
+var A;
+A = 660;
+function beep(freq, duration, vol) {
+    var context = new(window.AudioContext || window.webkitAudioContext);
+    const oscillator = context.createOscillator();
+    const gain = context.createGain();
+    gain.gain.setValueAtTime(0, context.currentTime);
+    gain.gain.linearRampToValueAtTime(1, context.currentTime + 0.002);
+    oscillator.connect(gain);
+    oscillator.frequency.value = freq;
+    oscillator.type = "square";
+    gain.connect(context.destination);
+    oscillator.start(context.currentTime);
+    oscillator.stop(context.currentTime + duration * .001);
+    oscillator.onended = () => context.close();
+}
+beep(A, 5000, 30);
