4a2136ad5dc92b1bd1fdf221acc8834a7d2b15c3
omega-beta <140123838+omega-beta@users.noreply.github.com>
2023-12-19 16:01:43 -0500

    Update key.js

--- key.js
+++ key.js
@@ -1,11 +1,12 @@
-var A, B, C, D, E, F, G;
-A = 440;
-B = 493.886;
-C = 524;
-D = 587.33;
-E = 659.25;
-F = 698.45;
-G = 775.08;
+var A[], B[], C[], D[], E[], F[], G[];
+A[5] = 440;
+B[5] = 493.886;
+C[5] = 524;
+D[5] = 587.33;
+E[5] = 659.25;
+F[5] = 698.45;
+G[5] = 775.08;
+A[6] = 880;
 function beep(freq, duration, vol) {
     var context = new(window.AudioContext || window.webkitAudioContext);
     const oscillator = context.createOscillator();
