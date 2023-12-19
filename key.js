b6e27a1095b4b9dd89f2f6ef87f865f335bcd9a3
omega-beta <140123838+omega-beta@users.noreply.github.com>
2023-12-19 12:45:58 -0500

    Update key.js

--- key.js
+++ key.js
@@ -1,6 +1,6 @@
 var A;
 A = 660;
-function beep(freq = 660, duration = 90, vol = 50) {
+function beep(freq = 660, duration = 5000, vol = 50) {
     var context = new(window.AudioContext || window.webkitAudioContext);
     const oscillator = context.createOscillator();
     const gain = context.createGain();
